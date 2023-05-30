c77c9c0e932c08b0e2187bc7def2c730d03e2600
Fabio <fcielo.dev@gmail.com>
2023-05-30 10:41:34 +0200

    added character models to player and enemy

--- Kleptomaniac/Assets/AI/EnemyPatrolling.cs
+++ Kleptomaniac/Assets/AI/EnemyPatrolling.cs
@@ -17,7 +17,7 @@ public class EnemyPatrolling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position != points[current].position)
+        if (Mathf.Abs(transform.position.x - points[current].position.x) > 0.1f && Mathf.Abs(transform.position.z - points[current].position.z) > 0.1f)
         {
             transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
             transform.LookAt(points[current].position); // Aggiunta: Orienta l'EnemyCharacter verso il punto di destinazione
