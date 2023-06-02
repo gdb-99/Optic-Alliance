da669769476e6bb5893a39ae8b061e98de6da3f4
GhostOgre97 <GhostOgre97@users.noreply.github.com>
2023-06-02 19:58:25 +0200

    EnemyPatrolling2 versione 128736.0

--- Kleptomaniac/Assets/AI/EnemyPatrolling2.cs
+++ Kleptomaniac/Assets/AI/EnemyPatrolling2.cs
@@ -25,8 +25,8 @@ public class EnemyPatrolling2 : MonoBehaviour
     private bool CanSeePlayer => IsPlayerInVisionCone() && !IsPlayerObstructed();
     private Vector3 lastKnownPlayerPosition;
     private float searchTimer;
-    private float searchDuration = 5f;
-    [SerializeField] private float rotationSpeed = 1f; // Velocità di rotazione in gradi al secondo
+    [SerializeField] private float searchDuration = 5f;
+    [SerializeField] private float rotateSpeed = 60f;
 
     enum EnemyState
     {
@@ -85,57 +85,32 @@ public class EnemyPatrolling2 : MonoBehaviour
                     ReturnToLastKnownPlayerPosition();
                     if (agent.remainingDistance < 0.1f)
                     {
-                        //agent.enabled = false;
                         agent.angularSpeed = 0f;
                         searchTimer = 0f;
-                        //StartCoroutine(AnimateEnemy());
                         currentState = EnemyState.Searching;
                     }
-
-                    //searchTimer = 0f;
                 }
                 break;
 
             case EnemyState.Searching:
                 searchTimer += Time.deltaTime;
-                //agent.enabled = false;
                 if (CanSeePlayer)
                 {
-                    // agent.enabled = true;
                     agent.angularSpeed = 120f;
                     currentState = EnemyState.Chasing;
-                   // searchTimer = 0f;
                 }
                 if (searchTimer >= searchDuration)
                 {
                     agent.angularSpeed = 120f;
                     currentState = EnemyState.Patrolling;
-                    //searchTimer = 0f;
                 }
                 else
                 {
-                    //ReturnToLastKnownPlayerPosition();
-                    //DA MIGLIORARE
-                    LookAround();
-                    Debug.Log("Girotondo");
+                    transform.localEulerAngles = new Vector3(0, Mathf.PingPong(Time.time * rotateSpeed, 60) - 30, 0);
+                    Debug.Log("Forse è qui intorno...");
                 }
                 break;
         }
-    }
-    IEnumerator AnimateEnemy()
-    {
-        float rotationAngle = 120f;
-        for (int i = 0; i<3; i++)
-        {
-        // Calcola la velocità di rotazione in radianti al secondo
-        float rotationSpeedRad = rotationSpeed * Mathf.Deg2Rad;
-        //Debug.Log("Prima" + searchTimer);
-        // Ruota il nemico attorno all'asse verticale (asse Y)
-        transform.Rotate(Vector3.up, rotationAngle * rotationSpeedRad *Time.deltaTime, Space.Self);
-        yield return new WaitForSeconds(0.1f);
-        }
-
-
     }
 
     void LookAtPlayer()
@@ -161,35 +136,6 @@ public class EnemyPatrolling2 : MonoBehaviour
         agent.destination = lastKnownPlayerPosition;
     }
 
-    void LookAround()
-    {
-        float rotationAngle = 120f;
-        // Calcola la velocità di rotazione in radianti al secondo
-        float rotationSpeedRad = rotationSpeed * Mathf.Deg2Rad;
-            //Debug.Log("Prima" + searchTimer);
-            // Ruota il nemico attorno all'asse verticale (asse Y)
-            transform.Rotate(Vector3.up, rotationAngle * rotationSpeedRad * Time.deltaTime, Space.Self);
-        if (searchTimer >= searchDuration)
-        {
-            // Arresta la rotazione
-            transform.rotation = Quaternion.identity;
-        }
-        /*// Calcola l'angolo di rotazione desiderato per un giro completo
-        float rotationAngle = 60f;
-        // Calcola la velocità di rotazione in radianti al secondo
-        float rotationSpeedRad = rotationSpeed * Mathf.Deg2Rad;
-        //Debug.Log("Prima" + searchTimer);
-        // Ruota il nemico attorno all'asse verticale (asse Y)
-        transform.Rotate(Vector3.up, rotationAngle * Time.deltaTime);
-
-        //Debug.Log("Dopo" + searchTimer);
-        // Controlla se è trascorso il tempo per un giro completo
-        if (searchTimer >= searchDuration)
-        {
-            // Arresta la rotazione
-            transform.rotation = Quaternion.identity;
-        } */
-    }
 
 #if UNITY_EDITOR
     private void OnDrawGizmosSelected()
