95858ec6547aa870c486f52d82c8e55508bf9c8a
Dexef22 <100279341+Dexef22@users.noreply.github.com>
2023-07-10 12:16:50 +0200

    Game Over completed.

--- Kleptomaniac/Assets/Scripts/GameOver.cs
+++ Kleptomaniac/Assets/Scripts/GameOver.cs
@@ -5,22 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    //public void Resume() {
-
-    //    Cursor.lockState = CursorLockMode.Locked;
-    //    Cursor.visible = false;
-
-    //    pauseMenuUI.SetActive(false);
-    //    Time.timeScale = 1f;
-    //    GameIsPaused = false;
-    //}
-
+    
     public void Start() {
         Debug.Log("GAME OVER SCREEN REACHED");
     }
 
     public void RestartLevel() {
         Debug.Log("RESTARTING SCENE");
+
+        //Fai ripartire il gioco:
+        Time.timeScale = 1f;
+
+        //Carica la nuova scena:
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        //Togli il cursore:
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void QuitGame(){
+
+        Debug.Log("QUIT SCENE");
+
+        //Fai ripartire il gioco:
+        Time.timeScale = 1f;
+
+        //Carica la nuova scena:
+        SceneManager.LoadScene("SelectLevelScene");
+
+        //Mostra il cursore:
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
     }
 }
